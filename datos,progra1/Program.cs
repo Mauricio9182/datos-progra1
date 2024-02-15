aaf16b7672eab02ed200f9852f04bd4a7d4dffd0
mauricio9182 <pablocarrilopb0199@gmail.com>
2024-02-15 16:14:17 -0600

    Agregar archivos de proyecto.

---
+++ datos,progra1/Program.cs
@@ -0,0 +1,50 @@
+﻿/*int num1, num2, resultado;
+Console.WriteLine("ingrese el primer valor: ");
+num1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("ingrese el segundo valor: ");
+num2 = int.Parse(Console.ReadLine());
+
+resultado = num1 + num2;
+Console.WriteLine("el resultado es: " + resultado);*/
+
+/*int num1, num2, resultado;
+Console.WriteLine("ingrese el primer valor: ");
+num1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("ingrese el segundo valor: ");
+num2 = int.Parse(Console.ReadLine());
+
+resultado = num1 - num2;
+Console.WriteLine("el resultado es: " + resultado);*/
+
+/*int num1, num2, resultado;
+Console.WriteLine("ingrese el primer valor");
+num1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Ingrese el segundo valor");
+num2 = int.Parse(Console.ReadLine());
+
+resultado = num1 * num2;
+Console.WriteLine("el resultado es:" + resultado);*/
+
+
+/*int num1, num2, resultado;
+Console.WriteLine("ingrese el primer valor");
+num1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("ingrse el segundo valor");
+num2 = int.Parse(Console.ReadLine());
+
+resultado = num1 / num2;
+Console.WriteLine("el resultado es:" + resultado);*/
+
+/*int num1, num2, modulo;
+Console.WriteLine("Ingrese el primer número:");
+num1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Ingrese el segundo número:");
+num2 = int.Parse(Console.ReadLine());
+
+modulo = num1 % num2;
+Console.WriteLine("El módulo de los dos números es: " + modulo);*/
